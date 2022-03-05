57ad9069127fcb64dd4c244032449dc86b00ebbd
Ruchita Senjaliya <senjaliyaruchita@gmail.com>
2022-03-05 11:07:51 +0530

    navbar is fixed at top

--- src/components/News.js
+++ src/components/News.js
@@ -1,57 +1,66 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
+
 const News = (props) => {
 
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(1);
+  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(1)
   const [totalResults, setTotalResults] = useState(0);
-
-  // document.title = `${capitalizeFirstLetter(
+  // document.title = `${this.capitalizeFirstLetter(
   //   props.category
   // )} - NewsMonkey`;
 
   const updateNews = async () => {
-    props.setProgress(0);
-    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=${props.apiKey} &page=1&pageSize=${props.pageSize}`;
-    setLoading(true)
+    props.setProgress(10);
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=${props.apiKey}&page=${page+ 1}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url);
     props.setProgress(30);
     let parsedData = await data.json();
     props.setProgress(70);
-
     setArticles(parsedData.articles)
     setTotalResults(parsedData.totalResults)
-    setLoading(false)
-
     props.setProgress(100);
+    setLoading(false)
   }
+
   useEffect(() => {
-    updateNews();
+        updateNews(); 
+        // eslint-disable-next-line
   }, [])
+
+  // handlePrevClick = async() => {
+  //   this.setState({page: this.state.page - 1});
+  //   this.updateNews();
+  // };
+
+  // handleNextClick = async () => {
+  //   this.setState({page: this.state.page + 1});
+  //   this.updateNews();
+  // };
 
   const capitalizeFirstLetter = (string) => {
     return string?.charAt(0).toUpperCase() + string.slice(1);
   };
-  const fetchMoreData = async () => {
-    setPage(page + 1)
-    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=${props.apiKey}&page=1&pageSize=${props.pageSize}`;
 
-    setLoading(true)
+
+
+  const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
+    setPage(page+1) 
     let data = await fetch(url);
-    let parsedData = await data.json();
+    let parsedData = await data.json()
     setArticles(articles.concat(parsedData.articles))
     setTotalResults(parsedData.totalResults)
-    setLoading(false)
   };
-
   return (
     <>
-      <h1 className="mb-4 text-center mt-4">NewsMonkey - Top headlines </h1>
+      <h1 className="text-center " style={{marginTop: "67px"}}>NewsMonkey - Top headlines </h1>
       {loading && <Spinner />}
       <InfiniteScroll
         dataLength={articles.length}
@@ -82,8 +91,6 @@ const News = (props) => {
     </>
   );
 }
-export default News
-
 News.defaultProps = {
   category: "general",
   country: "in",
@@ -94,3 +101,4 @@ News.PropType = {
   country: PropTypes.string,
   pageSize: PropTypes.number,
 };
+export default News
